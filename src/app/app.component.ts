56791d5bc48d900b33a8c945e28107ac0e735b22
Fabian Sainz <sainzfabian01@gmail.com>
2024-09-13 18:48:33 -0600

    Primer commit basesAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basesAngular';
+
+  pelicula={
+    nombre: 'Deadpool',
+    fechaLanzamiento:new Date(),
+    precio: 123.65
+  }
+
+duplicarNumero(valor:number):number{
+  return valor*2;
+}
+
 }
