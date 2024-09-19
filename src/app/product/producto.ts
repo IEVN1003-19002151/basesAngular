28f5645818720b7dd2d01dc7dcbc914e49801868
Fabian Sainz <sainzfabian01@gmail.com>
2024-09-18 18:57:53 -0600

    18/09/24

---
+++ src/app/product/producto.ts
@@ -0,0 +1,11 @@
+export interface IProducto {
+
+    productoID:number;
+    Modelo:string;
+    Descripcion:string;
+    year:string;
+    Precio:number;
+    Marca:string;
+    Color:string;
+    ImagenUrl:string;
+}
